e3a0f19496897cd4ab2535440ca58269ab704ad1
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-09-07 08:33:34 -0300

    Add no-headers config to stack screens

---
+++ src/screens/Preload/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+  background-color: #63c2d1;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
