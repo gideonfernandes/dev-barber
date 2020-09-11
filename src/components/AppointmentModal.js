26086f8ff1f7770d110cee528efdd51918b1c225
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-09-11 17:53:55 -0300

    Add date scroll selection layout on AppointmentModal

--- src/components/AppointmentModal.js
+++ src/components/AppointmentModal.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components/native';
 import {useNavigation} from '@react-navigation/native';
 
 import ExpandIcon from '../assets/expand.svg';
+import NavPrevIcon from '../assets/nav_prev.svg';
+import NavNextIcon from '../assets/nav_next.svg';
 
 const Modal = styled.Modal``;
 
@@ -67,6 +69,58 @@ const ServicePrice = styled.Text`
   color: #444;
 `;
 
+const DateInfo = styled.View`
+  flex-direction: row;
+`;
+
+const DatePrevArea = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: flex-end;
+  align-items: flex-end;
+`;
+
+const DateTitleArea = styled.View`
+  width: 140px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const DateTitle = styled.Text`
+  font-size: 17px;
+  font-weight: bold;
+  color: #333;
+`;
+
+const DateNextArea = styled.TouchableOpacity`
+  flex: 1;
+  align-items: flex-end;
+`;
+
+const DateList = styled.ScrollView`
+  height: 100px;
+`;
+
+const DateItem = styled.TouchableOpacity`
+  width: 45px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  opacity: ${(props) => (props.available ? '0.3' : 1)};
+`;
+
+const DateItemWeekDay = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  color: #333;
+`;
+
+const DateItemNumber = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+`;
+
 const FinishButton = styled.TouchableOpacity`
   background-color: #333;
   height: 60px;
@@ -81,11 +135,88 @@ const FinishButtonText = styled.Text`
   color: #fff;
 `;
 
+const months = [
+  'Janeiro',
+  'Fevereiro',
+  'Março',
+  'Abril',
+  'Maio',
+  'Junho',
+  'Julho',
+  'Agosto',
+  'Setembro',
+  'Outubro',
+  'Novembro',
+  'Dezembro',
+];
+
+const days = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'];
+
 const AppointmentModal = ({show, setShow, user, service}) => {
+  const [selectedYear, setSelectedYear] = useState(0);
+  const [selectedMonth, setSelectedMonth] = useState(0);
+  const [selectedDay, setSelectedDay] = useState(0);
+  const [selectedHour, setSelectedHour] = useState(null);
+  const [listDays, setListDays] = useState([]);
+  const [listHours, setListHours] = useState([]);
+
+  useEffect(() => {
+    const today = new Date();
+    setSelectedYear(today.getFullYear());
+    setSelectedMonth(today.getMonth());
+    setSelectedDay(today.getDate());
+  }, []);
+
+  useEffect(() => {
+    if (user.available) {
+      let daysInMonth = new Date(selectedYear, selectedMonth + 1, 0).getDate();
+      let newListDays = [];
+
+      for (let i = 1; i <= daysInMonth; i++) {
+        let d = new Date(selectedYear, selectedMonth, i);
+        let year = d.getFullYear();
+        let month = d.getMonth() + 1;
+        let day = d.getDate();
+
+        month = month < 10 ? '0' + month : month;
+        day = day < 10 ? '0' + day : day;
+        let selDate = `${year}-${month}-${day}`;
+
+        let availability = user.available.filter((e) => e.date === selDate);
+
+        newListDays.push({
+          status: availability.length > 0 ? true : false,
+          weekDay: days[d.getDay()],
+          number: i,
+        });
+      }
+
+      setListDays(newListDays);
+      setSelectedDay(1);
+      setListHours([]);
+      setSelectedHour(0);
+    }
+  }, [user, selectedMonth, selectedYear]);
+
   const navigation = useNavigation();
 
   const handleCloseModal = () => {
     setShow(false);
+  };
+
+  const handleChangeMonth = (action) => {
+    const mountDate = new Date(selectedYear, selectedMonth, 1);
+    if (action === 'prev') {
+      mountDate.setMonth(mountDate.getMonth() - 1);
+    }
+
+    if (action === 'next') {
+      mountDate.setMonth(mountDate.getMonth() + 1);
+    }
+
+    setSelectedYear(mountDate.getFullYear());
+    setSelectedMonth(mountDate.getMonth());
+    setSelectedDay(1);
   };
 
   const handleFinishButton = () => {};
@@ -115,6 +246,37 @@ const AppointmentModal = ({show, setShow, user, service}) => {
               </ServiceInfo>
             </ModalItem>
           )}
+
+          <ModalItem>
+            <DateInfo>
+              <DatePrevArea onPress={() => handleChangeMonth('prev')}>
+                <NavPrevIcon width="35" height="35" fill="#333" />
+              </DatePrevArea>
+
+              <DateTitleArea>
+                <DateTitle>
+                  {months[selectedMonth]} {selectedYear}
+                </DateTitle>
+              </DateTitleArea>
+
+              <DateNextArea onPress={() => handleChangeMonth('next')}>
+                <NavNextIcon width="35" height="35" fill="#333" />
+              </DateNextArea>
+            </DateInfo>
+
+            {listDays.length > 0 && (
+              <DateList
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}>
+                {listDays.map((day, key) => (
+                  <DateItem key={key} available={day.status} onPress={() => {}}>
+                    <DateItemWeekDay>{day.weekDay}</DateItemWeekDay>
+                    <DateItemNumber>{day.number}</DateItemNumber>
+                  </DateItem>
+                ))}
+              </DateList>
+            )}
+          </ModalItem>
 
           <FinishButton onPress={handleFinishButton}>
             <FinishButtonText>Finalizar Agendamento</FinishButtonText>
